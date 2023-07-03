a0e3548d9fa44d92f7825ed2dcd58ce6e8cbcfbf
srg <skabassjob@gmail.com>
2023-07-04 02:48:15 +0400

    feat: add siple server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('I am CRUD API')
+import http from 'node:http'
+import { defaultPort, serverListenMessage } from './const'
+
+const port = process.env.PORT ? Number(process.env.PORT) : defaultPort
+
+const server = http.createServer()
+
+server.listen(port, () => {
+  console.info(`${serverListenMessage}: ${port}`)
+})
